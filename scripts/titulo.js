94fc01a50f76b9dde3483fa3c08f21491dfe6cad
Hydra <sistemaautodocs+github@gmail.com>
2024-07-08 10:27:34 -0300

    Atualização 1.1
    Título do documento baseado nos dados fornecidos.

---
+++ scripts/titulo.js
@@ -0,0 +1,7 @@
+document.getElementById('i-total').addEventListener('input', function() {
+    var cnpj = document.getElementById('cnpj').textContent;
+
+    var total = document.getElementById('total').textContent;
+
+    document.title = "ASJ Assessoria [Orçamento R$ " + total + "] - " + cnpj;
+});
